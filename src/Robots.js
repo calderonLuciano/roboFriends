e76c8d85bae77ecc6aa5e4c00fb1e74d80479aaa
calderonluciano <calderonlucianoa@gmail.com>
2018-06-24 17:16:12 -0300

    Initial commit

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Luciano',
+    username:'lu1491',
+    email:'lu_14_91@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Lucas',
+    username:'luc1491',
+    email:'luc_14_91@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Luciana',
+    username:'lua1491',
+    email:'lua_14_91@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Pepa',
+    username:'pepa',
+    email:'lu_14_91@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Ramona',
+    username:'ramona',
+    email:'lu_14_91@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Francisco',
+    username:'luad1491',
+    email:'luad_14_91@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Maria esperanza',
+    username:'lu1',
+    email:'lu_91@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Cecilia',
+    username:'u1491',
+    email:'lu14_91@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Luciano',
+    username:'lu1491',
+    email:'lu_14_91@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'Fran',
+    username:'asdf',
+    email:'lu_adfad1@gmail.com'
+  }
+]
